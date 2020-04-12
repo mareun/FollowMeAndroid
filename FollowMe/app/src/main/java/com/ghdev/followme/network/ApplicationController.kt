be5207dad30bbc34f4884babec982174981b0502
yougahee <dbrkgml97@nate.com>
2020-04-13 01:31:30 +0900

    signup connection

--- FollowMe/app/src/main/java/com/ghdev/followme/network/ApplicationController.kt
+++ FollowMe/app/src/main/java/com/ghdev/followme/network/ApplicationController.kt
@@ -1,4 +1,28 @@
 package com.ghdev.followme.network
 
-class ApplicationController {
+import android.app.Application
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApplicationController : Application() {
+
+    private val baseURL = "http://117.17.196.142:8008"
+    lateinit var networkService: NetworkService
+
+    companion object {
+        lateinit var instance: ApplicationController
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+        buildNetWork()
+    }
+
+    fun buildNetWork() {
+        val retrofit: Retrofit = Retrofit.Builder().baseUrl(baseURL).addConverterFactory(
+            GsonConverterFactory.create()
+        ).build()
+        networkService = retrofit . create (NetworkService::class.java)
+    }
 }
